ae5f8030532a7d628c7e46d94a2cd61cb74a7071
SelimHan46 <gedikselim99@gmail.com>
2023-08-15 13:38:43 +0300

    stork function

---
+++ Kaynak.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(void)
+{
+	
+	char s[] = " Ali, Veli, Selami    ";
+	char* pstr;
+	
+
+
+	pstr = strtok(s, ", ");
+
+	while (pstr != NULL) {
+		puts(pstr);
+		pstr = strtok(NULL, ", ");
+	}
+
+
+
+	return 0;
+}
