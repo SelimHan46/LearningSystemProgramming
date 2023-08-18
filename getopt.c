31f7bed2ef27828172994ac5f17abce2b3f46db5
SelimHan46 <gedikselim99@gmail.com>
2023-08-18 17:34:59 +0300

    reforme edilmiş hali

--- getopt.c
+++ getopt.c
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
 {
     int result;
     int a_flag, b_flag, c_flag;
+    char* b_arg;
 
     a_flag = b_flag = c_flag = 0;
 
@@ -18,6 +19,7 @@ int main(int argc, char* argv[])
             break;
         case 'b':
             b_flag = 1;
+            b_arg = optarg;
             break;
         case 'c':
             c_flag = 1;
@@ -28,7 +30,7 @@ int main(int argc, char* argv[])
     if (a_flag)
         printf("-a option used....\n");
     if (b_flag)
-        printf("-b option used...\n");
+        printf("-b option used with argument \"%s\"\n", b_arg);
     if (c_flag)
         printf("-c option used....\n");
 
